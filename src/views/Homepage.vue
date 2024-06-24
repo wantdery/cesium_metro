e18f126d2d45f669e4c49c9f8c27e5b527e7d5a9
bk <872850344@qq.com>
2024-06-24 14:14:13 +0800

    初始化重点活动

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script setup>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 </script>
 
 <style scoped>
