e41fa4ba7e9f9da9b82624a8ba802255724c04f4
ljj <2090115809@qq.com>
2024-06-24 12:01:27 +0800

    测试

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="Home">
     <Header />
+
     <Footer />
   </div>
 </template>
 
 <script setup>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 </script>
 
 <style scoped>
